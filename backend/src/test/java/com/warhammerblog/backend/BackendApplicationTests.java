3da0e8a092bc303093152f1ed12cf1592be764ea
Eds-Dbug <edsDbug@gmail.com>
2024-05-27 23:17:54 -0400

    Create new warhammer blog with Spring

---
+++ backend/src/test/java/com/warhammerblog/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warhammerblog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
